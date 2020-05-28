c264ab4ed0f01b620736b8d96ec722e275ee9635
Rich Wasek <rideburtonrw@gmail.com>
2020-05-28 16:09:20 -0600

    Added needed changes to host to AWS

--- MoreBetter/src/main/java/com/skilldistillery/morebetterapp/MoreBetterApplication.java
+++ MoreBetter/src/main/java/com/skilldistillery/morebetterapp/MoreBetterApplication.java
@@ -2,12 +2,16 @@ package com.skilldistillery.morebetterapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MoreBetterApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MoreBetterApplication.class, args);
-	}
-
+public class MoreBetterApplication extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(MoreBetterApplication.class);
+  }
+  public static void main(String[] args) {
+    SpringApplication.run(MoreBetterApplication.class, args);
+  }
 }
